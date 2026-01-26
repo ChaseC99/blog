9da4445d63a33ff9394d5430edaf2ecff81b137d
Chase Carnaroli <chasecarnaroli@gmail.com>
2026-01-25 22:43:13 -0800

    Update blog description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Chase's Blog";
-export const SITE_DESCRIPTION = 'Welcome to my Blog!';
+export const SITE_DESCRIPTION = 'I use this space to share thoughts, learnings, and my story';
