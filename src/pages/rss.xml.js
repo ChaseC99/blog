9b974c619d98f0beb7ee097c82103eac0aaf6318
Chase Carnaroli <chasecarnaroli@gmail.com>
2026-01-25 23:02:01 -0800

    Add Subscription Options
    - Create footer with email and rss links
    - Add footer to sitelayout
    - Set main min-height to keep footer at bottom
    - Sort RSS feed by publish date

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,17 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
+	const items = posts
+		.sort((a, b) => b.data.pubDate - a.data.pubDate)
+		.map((post) => ({
+			...post.data,
+			link: `/blog/${post.id}/`,
+		}));
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items: items,
 	});
 }
